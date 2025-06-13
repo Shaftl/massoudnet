f4538e808a52cafd3402d11cbcd05758684d5189
Shaftl <shaftlkhan990@gmail.com>
2025-06-13 09:11:26 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
-import path from "path";
-import { withPWA } from "next-pwa";
+const path = require("path");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
   experimental: {
     turbo: { loaders: true },
   },
   images: {
     domains: ["res.cloudinary.com"],
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve("./");
     return config;
   },
-});
+};
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
